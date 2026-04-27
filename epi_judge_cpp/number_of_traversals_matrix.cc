4c4923dd3c0262b3726345e0cb94b84383551087
Jesse-Jackson Zigi <zigi.jesse-jackson@turntabl.io>
2026-04-27 16:10:58 +0000

    num_traversals solved

--- epi_judge_cpp/number_of_traversals_matrix.cc
+++ epi_judge_cpp/number_of_traversals_matrix.cc
@@ -1,8 +1,14 @@
 #include "test_framework/generic_test.h"
-
+#include <vector>
 int NumberOfWays(int n, int m) {
-  // TODO - you fill in here.
-  return 0;
+  std::vector<std::vector<int>> dp_matrix( n, std::vector<int>(m, 1));
+  if(n ==1 || m == 1) return 1;
+  for(int i = 1; i<n;i++){
+    for(int j = 1;j<m;j++){
+      dp_matrix[i][j] = dp_matrix[i-1][j]+dp_matrix[i][j-1];
+    }
+  }
+  return dp_matrix[n-1][m-1];
 }
 
 int main(int argc, char* argv[]) {
