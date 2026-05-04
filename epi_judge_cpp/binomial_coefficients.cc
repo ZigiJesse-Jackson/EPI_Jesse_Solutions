4d244146728de7455905407465d2f06ff3b913b3
Jesse-Jackson Zigi <zigi.jesse-jackson@turntabl.io>
2026-05-04 12:40:18 +0000

    binomial coefficients

--- epi_judge_cpp/binomial_coefficients.cc
+++ epi_judge_cpp/binomial_coefficients.cc
@@ -1,8 +1,28 @@
 #include "test_framework/generic_test.h"
+#include <vector>
 
 int ComputeBinomialCoefficient(int n, int k) {
-  // TODO - you fill in here.
-  return 0;
+  if(k==0 ) return 1;
+  if(n==0) return 0;
+  
+  std::vector<std::vector<int>> dp(k,std::vector<int>(n, 0));
+  for(int i=0;i<k;i++){
+    for(int j=0;j<n;j++){
+      if(i==0){
+        dp[i][j] = j+1;
+      }
+      else if(i==j){
+        dp[i][j] = 1;
+      }
+      else if(j==0){
+        continue;
+      }
+      else{
+        dp[i][j] = dp[i][j-1]+dp[i-1][j-1];
+      }
+    }
+  }
+  return dp[k-1][n-1];
 }
 
 int main(int argc, char* argv[]) {
