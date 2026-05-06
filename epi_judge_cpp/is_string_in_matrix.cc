8783914e5b1c2d20556d35e10fff14397e9fe08e
Jesse-Jackson Zigi <zigi.jesse-jackson@turntabl.io>
2026-05-06 15:37:44 +0000

    is string in matrix solved

--- epi_judge_cpp/is_string_in_matrix.cc
+++ epi_judge_cpp/is_string_in_matrix.cc
@@ -2,13 +2,65 @@
 
 #include "test_framework/generic_test.h"
 using std::vector;
-bool IsPatternContainedInGrid(const vector<vector<int>>& grid,
-                              const vector<int>& pattern) {
-  // TODO - you fill in here.
-  return true;
+bool IsPatternContainedInGrid(const vector<vector<int>> &grid,
+                              const vector<int> &pattern)
+{
+
+  vector<vector<vector<bool>>> dp(pattern.size(), vector<vector<bool>>(grid.size(), vector<bool>(grid[0].size(), false)));
+
+  for (int i = 0; i < pattern.size(); i++)
+  {
+    for (int r = 0; r < grid.size(); r++)
+    {
+      for (int c = 0; c < grid[0].size(); c++)
+      {
+        // base case: start of sequence found in grid
+        if (i == 0)
+        {
+          if (grid[r][c] == pattern[i])
+          {
+            dp[i][r][c] = true;
+          }
+          if (i == pattern.size() - 1 && dp[i][r][c])
+          {
+            return dp[i][r][c];
+          }
+          continue;
+        }
+        // relation: current sequence character found in grid
+        if (pattern[i] == grid[r][c])
+        {
+          if (r + 1 < grid.size())
+          {
+            dp[i][r][c] = dp[i - 1][r + 1][c] || dp[i][r][c];
+          }
+          if (r - 1 >= 0)
+          {
+            dp[i][r][c] = dp[i - 1][r - 1][c] || dp[i][r][c];
+          }
+          if (c + 1 < grid[0].size())
+          {
+            dp[i][r][c] = dp[i - 1][r][c + 1] || dp[i][r][c];
+          }
+          if (c - 1 >= 0)
+          {
+            dp[i][r][c] = dp[i - 1][r][c - 1] || dp[i][r][c];
+          }
+
+          if (i == pattern.size() - 1 && dp[i][r][c])
+          {
+            return dp[i][r][c];
+          }
+        }
+      }
+    }
+  }
+
+  return false;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"grid", "pattern"};
   return GenericTestMain(args, "is_string_in_matrix.cc",
