6904308c2f36c4ec5193696e04161541590302f5
Jesse-Jackson Zigi <59916143+ZigiJesse-Jackson@users.noreply.github.com>
2026-08-17 22:00:51 +0000

    soln majority element, max trapped water, refueling schedule

--- epi_judge_cpp/max_trapped_water.cc
+++ epi_judge_cpp/max_trapped_water.cc
@@ -3,12 +3,27 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 
-int GetMaxTrappedWater(const vector<int>& heights) {
-  // TODO - you fill in here.
-  return 0;
+int GetMaxTrappedWater(const vector<int> &heights)
+{
+  int l = 0, r = heights.size() - 1;
+  int max_trapped = 0;
+  while (l < r)
+  {
+    max_trapped = std::max(max_trapped, (std::min(heights[l], heights[r]) * (r - l)));
+    if (heights[l] < heights[r])
+    {
+      l++;
+    }
+    else
+    {
+      r--;
+    }
+  }
+  return max_trapped;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"heights"};
   return GenericTestMain(args, "max_trapped_water.cc", "max_trapped_water.tsv",
--- epi_judge_cpp/refueling_schedule.cc
+++ epi_judge_cpp/refueling_schedule.cc
@@ -9,25 +9,66 @@ const int kMPG = 20;
 
 // gallons[i] is the amount of gas in city i, and distances[i] is the distance
 // city i to the next city.
-int FindAmpleCity(const vector<int>& gallons, const vector<int>& distances) {
-  // TODO - you fill in here.
-  return 0;
+// int FindAmpleCity(const vector<int> &gallons, const vector<int> &distances)
+// {
+
+//   for (int i = 0; i < distances.size(); i++)
+//   {
+//     double tank = gallons[i];
+//     if (tank < distances[i] / 20.0)
+//       continue;
+//     int x = i + 1 < distances.size() ? i + 1 : 0;
+//     tank -= distances[i] / 20.0;
+//     while (i != x)
+//     {
+//       // refill
+//       tank += gallons[x];
+//       tank -= distances[x] / 20.0;
+//       if (tank < 0)
+//         break;
+//       x = x + 1 < distances.size() ? x + 1 : 0;
+//     }
+//     if (i == x)
+//       return i;
+//   }
+//   return 0;
+// }
+
+int FindAmpleCity(const vector<int> &gallons, const vector<int> &distances)
+{
+
+  int current_gas = 0, start_idx = 0;
+  for (int i = 0; i < distances.size(); i++)
+  {
+    current_gas += (gallons[i] - (distances[i] / 20));
+    if (current_gas < 0)
+    {
+      current_gas = 0;
+      start_idx = i + 1;
+    }
+  }
+  return start_idx;
 }
-void FindAmpleCityWrapper(TimedExecutor& executor, const vector<int>& gallons,
-                          const vector<int>& distances) {
-  int result = executor.Run([&] { return FindAmpleCity(gallons, distances); });
+void FindAmpleCityWrapper(TimedExecutor &executor, const vector<int> &gallons,
+                          const vector<int> &distances)
+{
+  int result = executor.Run([&]
+                            { return FindAmpleCity(gallons, distances); });
   const int num_cities = gallons.size();
   int tank = 0;
-  for (int i = 0; i < num_cities; ++i) {
+  for (int i = 0; i < num_cities; ++i)
+  {
     int city = (result + i) % num_cities;
     tank += gallons[city] * kMPG - distances[city];
-    if (tank < 0) {
+    if (tank < 0)
+    {
       throw TestFailure(FmtStr("Out of gas on city {}", i));
     }
   }
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"executor", "gallons", "distances"};
   return GenericTestMain(args, "refueling_schedule.cc",
